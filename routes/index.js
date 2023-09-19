b019cf619c2ea9d8579ea60a654dfa62f50b6f47
MatthewBurrisAU <zzed20xx@gmail.com>
2023-09-20 01:43:27 +1000

    remove components

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const getcount = require('../components/getcount')
-const newcount = require('../components/newcount')
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
